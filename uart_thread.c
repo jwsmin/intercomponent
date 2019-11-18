d0a9a6a4b118a8c15f87efd6bd40adbd15114d43
megans11 <40861278+megans11@users.noreply.github.com>
2019-11-17 20:07:17 -0500

    Add files via upload

---
+++ uart_thread.c
@@ -0,0 +1,27 @@
+
+
+#include "uart_thread.h"
+
+/*
+ *  ======== uartThread ========
+ */
+void *uartThread(void *arg0)
+{
+    // turn usb uart
+    createUartQueue();
+//    dbgUARTInit();
+
+    // Command variables
+    char msg[UART_QUEUE_WIDTH] = "\r\nUart thread opened\r\n\0";
+    dbgUARTVal(msg);
+
+    while (1) {
+        // blocking read from queue
+        readMsqFromUartQueue(msg);
+
+        // write to UART
+        dbgUARTVal(msg);
+    }
+}
+
+
